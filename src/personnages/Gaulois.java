fa37d66ddb75eaede1c5462761bfbee3a6a30e7f
Vazio07 <valerian.de.carvalho@gmail.com>
2023-09-21 15:29:23 +0200

    Fin tp1

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,30 +3,41 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
-	private int effetpotion = 1;
-	
+	private int effetPotion = 1;
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+	}
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
 	}
 
 	public int getForce() {
 		return force;
 	}
-
 	public void setForce(int force) {
 		this.force = force;
 	}
-
-	public int getEffetpotion() {
-		return effetpotion;
+	
+	public int getEffetPotion() {
+		return effetPotion;
 	}
+	public void setEffetPotion(int effetPotion) {
+		this.effetPotion = effetPotion;
+	}
+	
 
 	@Override
 	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetpotion=" + effetpotion + "]";
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
 	}
-	
+
+
 	public static void main(String[] args) {
 		
 	}
